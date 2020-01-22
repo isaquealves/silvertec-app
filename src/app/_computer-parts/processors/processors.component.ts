fa6cf0d76c51f9ecf2866dde3bb83ce03d0aeafd
Isaque Alves <isaquealves@gmail.com>
2020-01-22 06:21:36 -0300

    Implement processor component form

--- src/app/_computer-parts/processors/processors.component.ts
+++ src/app/_computer-parts/processors/processors.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ProcessorService } from '@/_services/processor.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-processors',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./processors.component.sass']
 })
 export class ProcessorsComponent implements OnInit {
-
-  constructor() { }
+  processors: any;
+  @Input() ordForm: FormGroup;
+  proc: any;
+  constructor(
+    private processorService: ProcessorService
+  ) { }
 
   ngOnInit() {
+    this.loadProcessors();
+  }
+
+  loadProcessors() {
+    this.processorService.getAll()
+      .pipe(first())
+      .subscribe(processors => this.processors = processors);
+  }
+
+  submitProcessor() {
+    this.ordForm.get('processorStep').get('processor').markAsTouched();
+    this.ordForm.get('processorStep').get('processor').updateValueAndValidity();
   }
 
 }
