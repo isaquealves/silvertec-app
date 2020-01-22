8c760ae16be7b3cfdf2c0ccdee6e6bfee7a1e421
Isaque Alves <isaquealves@gmail.com>
2020-01-22 06:23:44 -0300

    Implement memory selection component and service

--- src/app/_computer-parts/memory/memory.component.ts
+++ src/app/_computer-parts/memory/memory.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MemoryService } from '@/_services/memory.service';
+import { first } from 'rxjs/operators';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-memory',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemoryComponent implements OnInit {
 
-  constructor() { }
+  memorymodules: any;
+  @Input() ordForm: FormGroup;
+  modmem: any;
+
+  constructor(
+    private memoryService: MemoryService
+  ) { }
 
   ngOnInit() {
+    this.loadMemoryModules();
+  }
+
+  loadMemoryModules() {
+    this.memoryService.getAll()
+      .pipe(first())
+      .subscribe(rammodules => this.memorymodules = rammodules);
+  }
+
+  submitMem() {
+    console.log(this.ordForm.get('memoryStep').get('memorymodules').valueChanges);
+    this.ordForm.get('memoryStep').get('memorymodules').markAsTouched();
+    this.ordForm.get('memoryStep').get('memorymodules').updateValueAndValidity();
   }
 
 }
