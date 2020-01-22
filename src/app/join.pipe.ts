5bef1d5ed21a2640fbeef883c8650c879d28f4a5
Isaque Alves <isaquealves@gmail.com>
2020-01-22 15:07:09 -0300

    Add simple join pipe, useful for concat arrays

---
+++ src/app/join.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'join'
+})
+export class JoinPipe implements PipeTransform {
+
+  transform(value: string[]): string {
+    return value.join(', ');
+  }
+
+}
