cea16f33429a6183385718080c7cfbdf74ae2b46
Isaque Alves <isaquealves@gmail.com>
2020-01-22 06:22:33 -0300

    Implement mainboard component and service

--- src/app/_computer-parts/mainboards/mainboards.component.ts
+++ src/app/_computer-parts/mainboards/mainboards.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Component, OnInit, Input } from '@angular/core';
+import { MainboardService } from '@/_services/mainboard.service';
 
 @Component({
   selector: 'app-mainboards',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mainboards.component.sass']
 })
 export class MainboardsComponent implements OnInit {
+  mainboards: any;
+  mboard: any;
+  @Input() ordForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private mainboardService: MainboardService
+  ) { }
 
   ngOnInit() {
+    this.loadMainboards();
   }
 
+  loadMainboards() {
+    this.mainboardService.getAll()
+      .pipe(first())
+      .subscribe(mainboards => this.mainboards = mainboards);
+  }
+
+  submitMainboard() {
+    this.ordForm.get('mainboardStep').get('mainboard').markAsTouched();
+    this.ordForm.get('mainboardStep').get('mainboard').updateValueAndValidity();
+  }
 }
